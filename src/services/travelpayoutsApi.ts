3b5a233dae74650a59f965173ecdb8063681839d
Waseem Samra <waseemsamra@gmail.com>
2026-01-10 23:26:14 +0000

    samee

--- src/services/travelpayoutsApi.ts
+++ src/services/travelpayoutsApi.ts
@@ -65,15 +65,15 @@ if (!API_TOKEN) {
 
 class TravelpayoutsApiService {
   private static instance: TravelpayoutsApiService;
-  private flightSearchApiV2: AxiosInstance;
+  private flightSearchApi: AxiosInstance;
   private cache: {
     airports: Airport[] | null;
     lastUpdated: number;
   };
 
   private constructor() {
-    this.flightSearchApiV2 = axios.create({
-        baseURL: API_BASE, // Correct base URL
+    this.flightSearchApi = axios.create({
+        baseURL: API_BASE,
         timeout: 30000,
     });
 
@@ -134,16 +134,14 @@ class TravelpayoutsApiService {
           cabin_class: cabinClassMapping[params.cabin_class || 'economy'],
       };
       
-      // Use the full path from the base URL
-      const response = await this.flightSearchApiV2.post('/v2/prices/create_search', searchPayload, { 
+      const response = await this.flightSearchApi.post('/v2/prices/create_search', searchPayload, { 
         headers: this.getApiHeaders() 
       });
       return response.data.search_id;
   }
 
   async getFlightSearchResults(searchId: string): Promise<any> {
-      // Use the full path from the base URL
-      const response = await this.flightSearchApiV2.get('/v2/prices/search-results', {
+      const response = await this.flightSearchApi.get('/api/v3/flights_search_results', {
           params: { uuid: searchId },
           headers: this.getApiHeaders()
       });
