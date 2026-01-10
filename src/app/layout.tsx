365f4490084d444047aa739c2191c329f296b04f
Waseem Samra <waseemsamra@gmail.com>
2026-01-10 21:15:46 +0000

    noww error in header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Header } from '@/components/layout/header';
-import { Footer } from '@/components/layout/footer';
 import { Toaster } from "@/components/ui/toaster";
 import { Providers } from '@/components/providers';
 import { TravelProvider } from '@/context/TravelContext';
@@ -30,7 +29,6 @@ export default function RootLayout({
             <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex-grow">{children}</main>
-            <Footer />
             </div>
             <Toaster />
           </TravelProvider>
