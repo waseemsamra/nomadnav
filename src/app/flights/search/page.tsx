cdfc288d325b752de3f0b75b1297aba4c2da6cad
Waseem Samra <waseemsamra@gmail.com>
2026-01-12 00:34:07 +0000

    just make this

--- src/app/flights/search/page.tsx
+++ src/app/flights/search/page.tsx
@@ -315,7 +315,7 @@ function SearchResultsContent() {
                   </div>
               </div>
 
-              <Accordion type="multiple" className="w-full border-t mt-4" defaultValue={['Numbers of stops', 'Baggage', 'Airlines', 'Travel time']}>
+              <Accordion type="multiple" className="w-full border-t mt-4" defaultValue={['Numbers of stops', 'Baggage', 'Airlines', 'Duration of stops']}>
                   <FilterSection title="Numbers of stops" disabled={stopOptions.length === 0}>
                       <div className="space-y-2 pr-2">
                            <div className="flex items-center justify-between">
@@ -384,18 +384,8 @@ function SearchResultsContent() {
                           </div>
                       </div>
                   </FilterSection>
-
-                   <FilterSection title="Duration of stops" disabled>
-                     <p>Duration of stops filter content</p>
-                  </FilterSection>
-                   <FilterSection title="Airfares" disabled>
-                     <p>Airfares filter content</p>
-                  </FilterSection>
-                  <FilterSection title="Departure/Arrival times" disabled>
-                     <p>Time filter content</p>
-                  </FilterSection>
                   
-                  <FilterSection title="Travel time" disabled={durationRange.max === 0}>
+                  <FilterSection title="Duration of stops" disabled={durationRange.max === 0}>
                       <div className="p-2">
                         <p className="text-sm text-center mb-2">
                             {formatDuration(selectedDuration[0])} - {formatDuration(selectedDuration[1])}
@@ -410,8 +400,15 @@ function SearchResultsContent() {
                         />
                       </div>
                   </FilterSection>
+
+                   <FilterSection title="Airfares" disabled>
+                     <p>Airfares filter content</p>
+                  </FilterSection>
+                  <FilterSection title="Departure/Arrival times" disabled>
+                     <p>Time filter content</p>
+                  </FilterSection>
                   
-                  <FilterSection title="Connecting airports" disabled>
+                   <FilterSection title="Connecting airports" disabled>
                      <p>Connecting airports filter content</p>
                   </FilterSection>
 
@@ -678,5 +675,3 @@ export default function SearchResultsPage() {
     </Suspense>
   );
 }
-
-    
